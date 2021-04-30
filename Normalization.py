125fe16515f0c876a52a96a54c455c8483459fd2
Sachin Vishwakarma <sachin.vish91@gmail.com>
2021-04-30 10:48:21 +0200

    Update Normalization.py

--- Normalization.py
+++ Normalization.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 #Normalize the data this will return the value between 0 and 1
-def min_max_scaler(x,overall_min,overall_max):
+def Normalization(x,overall_min,overall_max):
     #overall_min = 45 # specify the global minimun from all the datasets
     #overall_max = 800 # specify the global maximum from all the datasets
     x = (x - overall_min)/(overall_max - overall_min)
@@ -11,7 +11,7 @@ def min_max_scaler(x,overall_min,overall_max):
     return x
 
 #To denormalize the data to original values from the normalized data
-def de_normalize(x,overall_min,overall_max):
+def De_Normalize(x,overall_min,overall_max):
     #overall_min = 45 # specify the global minimun from all the datasets
     #overall_max = 800 # specify the global maximum from all the datasets
     x = (x * (overall_max - overall_min) + overall_min)
